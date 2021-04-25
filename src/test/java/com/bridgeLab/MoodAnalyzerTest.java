27d82aace5946c7a13909c6481653ccedfbe53c8
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-25 10:15:33 +0530

    [Added] Refactor the code using Default and parameterized Constructor

--- src/test/java/com/bridgeLab/MoodAnalyzerTest.java
+++ src/test/java/com/bridgeLab/MoodAnalyzerTest.java
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
     //UC1 TC-1.1
+    //UC1 Refactor code using default and parameterized constructor TC-1.1
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Sad");
         String mood = moodAnalyzer.analyseMood("This is Sad Message");
         Assertions.assertEquals("SAD",mood);
     }
     //UC1 TC-1.2
+    //UC1 Refactor code using default and parameterized constructor TC-1.1
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Happy");
         String mood = moodAnalyzer.analyseMood("This is Happy Message");
         Assertions.assertEquals("HAPPY", mood);
     }
